b23fa5650bf0128a0c03a4204fb1e2439ffcd8d1
littl-rob <llittlerob@gmail.com>
2022-12-20 20:35:47 -0500

    updating

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -1,40 +1,103 @@
 import React from 'react';
 import clsx from 'clsx';
+import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
 import styles from './index.module.css';
 
+// images are from undraw and use the HEX code #a79886 
+const FeatureList = [
+  {
+    title: 'Playlists', // Homepage Icon Label
+   // imageUrl: '/img/docusaurus.png', //path to image in static img folder
+
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default, // Homepage Undraw Image
+  // Svg: require('@site/static/img/landing/brunch.svg').default, // Homepage Undraw Image
+    url: 'https://betty-cracker-recipes.github.io/BC-Recipes/docs/brunch-bar/brunch-landing/', //Link to Landing Page for Corresponding Recipe Section
+  },
+  
+/*   {
+    title: 'Photo Albums',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default, // Homepage Undraw Image
+   // Svg: require('@site/static/img/landing/dinner.svg').default,
+    url: 'https://betty-cracker-recipes.github.io/BC-Recipes/docs/dinner-bar/dinner-landing/',
+  },
+  {
+    title: 'Dessert Recipes',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default, // Homepage Undraw Image
+    url: 'https://betty-cracker-recipes.github.io/BC-Recipes/docs/dessert-bar/dessert-landing/',
+  }, */
+];
+
+// puts the above list into a function so that the title and svg appear in the correct order, are formatted and link to the URLs
+function Feature({Svg, title, url}) {
+  const svgUrl = (Svg);
+  return (
+    <div className={clsx('col col--4')}>
+        <Link to={url}>
+     {Svg && (
+          <div className="text--center">
+            <Svg className={styles.featureSvg} alt={title} />
+          </div> 
+      )}
+      <div className="text--center padding-horiz--md"> 
+        <h3>{title}</h3>
+      </div>
+      </Link>
+      
+    </div>
+  );
+} 
+
+//Formats the Homepage Header, use to edit website title on homescreen, tagline, email sign up button
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
+        {/* titles & subtitles pulled from site config so they only need to be changed in one place*/}
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
+
+         {/* icons in homepage banner*/}
+{/*          <img className={styles.heroImg} src="img/docusaurus.png" /> 
+         <img className={styles.heroImg} src="img/docusaurus.png" /> 
+         <img className={styles.heroImg} src="img/docusaurus.png"  />   */}
+         
+         {/*button text & link*/}
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="https://vsco.co/jessica-rob/gallery">
+            VSCO
           </Link>
-        </div>
+        </div>            
       </div>
+
     </header>
   );
 }
 
+// makes the content above display to the interface so that it can actually be seen, mapping of above features/ images in list to order them etc.
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+ const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+      title={`${siteConfig.title}`}>
+        <HomepageHeader/>
       <main>
-        <HomepageFeatures />
+        {FeatureList && FeatureList.length > 0 && (
+         <section className={styles.features}>
+          <div className="container">
+            <div className="row">
+              {FeatureList.map((props, idx) => (
+                <Feature key={idx} {...props} />
+              ))}
+            </div>
+          </div>
+        </section>
+        )}
       </main>
     </Layout>
   );
